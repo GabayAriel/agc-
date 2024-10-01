683025ed0585b6088d109f0c91a2e6a40b5dc079
Ariel Gabay <o0o0.wvvw.0o0o@exgmail.com>
2024-10-01 10:47:13 +0300

    add program class

--- Progect/Program.cs
+++ Progect/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] srgs)
+        {
+            Console.WriteLine("pls enter a number:");
+            double num1 = CheckInput.CheckTypeNumber(Console.ReadLine());
+            //
+            Console.WriteLine("pls enter a operator:");
+            string oper = CheckInput.CheckTypeOperator(Console.ReadLine());
+            //
+            Console.WriteLine("pls enter a number:");
+            double num2 = CheckInput.CheckTypeNumber(Console.ReadLine());
+            //
+            string result = Calculator.Calculate(num1, num2, oper);
+            Console.WriteLine("result:\n" + result);
+        }
+    }
+}
+
+
